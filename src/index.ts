46b5352c9b356d08abeabed4ca32610ea64a775d
Stepan <kurakste@gmail.com>
2020-08-09 23:51:58 +0300

    add main game object

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,12 @@
-import imgPath from './assets/img/mainHero2.png';
-import imageLoader from './helpers/imageLoader';
+import mainGame from './mainGameUnit/index';
 
-function addCanvasElement():HTMLCanvasElement {
-  const can:HTMLCanvasElement = document.createElement('canvas');
-  can.width = 600;
-  can.height = 1000;
-  can.style.background = 'black';
-  return can;
-}
 
-async function draw(ctx: CanvasRenderingContext2D): Promise<void> {
-  const img: any = await imageLoader(imgPath);
-  ctx.drawImage(img, 0, 0, 312, 312, 20, 20, 90, 90);
 
-}
+
 
 (async function() {
-  const canvas: HTMLCanvasElement = addCanvasElement();
-  const ctx = canvas.getContext('2d');
-  await draw(ctx);
+  const game = new mainGame(400, 600);
+  await game.initGame();
+  game.draw();
 
-  document.body.appendChild(canvas);
-
-  console.log('hello!');
 })();
