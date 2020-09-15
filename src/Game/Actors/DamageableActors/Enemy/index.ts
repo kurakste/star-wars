9a36559c3bdc58da41ec77d6b8e6d05fcfc96e78
Stepan <kurakste@gmail.com>
2020-09-15 23:15:35 +0300

    clean up

--- src/Game/Actors/DamageableActors/Enemy/index.ts
+++ src/Game/Actors/DamageableActors/Enemy/index.ts
@@ -30,7 +30,6 @@ class Enemy extends DamageableActors {
     const {dx, dy} = calcEnemySteps(this.level, {x: this.xpos, y: this.ypos});
     this.ypos = this.ypos + dy;
     const border = (this.xpos + dx >= this.game.width - this.width || this.xpos + dx <= 5);
-    console.log('-----border:', border, this.xpos + dx);
     if (border) return;
     this.xpos = this.xpos + dx;
   }
