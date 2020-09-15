82b5fb2ce3ad35c725fc115242ce8f1a501fe2f1
Stepan <kurakste@gmail.com>
2020-09-15 20:45:53 +0300

    change game speed

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import MainGame from './Game/mainGameUnit';
   const game = new MainGame(400, 900);
   await game.initGame();
   
-  setInterval(game.clock, 50);
+  setInterval(game.clock, 30);
   window.addEventListener('keydown',(e:KeyboardEvent)=>game.keyboardHandler(e));
   window.addEventListener('keyup',(e:KeyboardEvent)=>game.keyboardHandler(e));
 
